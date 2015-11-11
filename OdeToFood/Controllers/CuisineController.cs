c4d7629583a616d8f1ed25174551f45c51755d65
c.neculita <c.neculita@levi9.com>
2015-11-11 15:29:05 +0200

    Start up

---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Filters;
+
+namespace OdeToFood.Controllers
+{
+    [Log]
+    public class CuisineController : Controller
+    {
+        //
+        // GET: /Cuisine/
+       
+        public ActionResult Search(string name)
+        {
+            var message = name;
+            return Content(message);
+        }
+     
+
+
+    }
+}
---
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        //
+        // GET: /Reviews/
+
+        public ActionResult Index()
+        {
+            var model = from r in _reviews
+                        orderby r.Country
+                        select r;
+           
+            return View(model);
+           
+        }
+
+        //
+        // GET: /Reviews/Details/5
+
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// GET: /Reviews/Create
+
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /Reviews/Create
+
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        ////
+        //// GET: /Reviews/Edit/5
+
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /Reviews/Edit/5
+
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        ////
+        //// GET: /Reviews/Delete/5
+
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /Reviews/Delete/5
+
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        private static List<RestaurantReview> _reviews = new List<RestaurantReview>
+        {
+            new RestaurantReview
+            {
+                Id = 1,
+                Name = "Buenavista",
+                City = "Iasi",
+                Country = "Romania",
+                Rating = 7,
+            },
+            new RestaurantReview
+            {
+                Id = 1,
+                Name = "Toujours",
+                City = "Iasi",
+                Country = "Usa",
+                Rating = 10,
+            },
+            new RestaurantReview
+            {
+                Id = 1,
+                Name = "McDonalds",
+                City = "Iasi",
+                Country = "France",
+                Rating = 5,
+            }
+        };
+
+    }
+}
